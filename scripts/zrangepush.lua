d5d6bf9555a0d730be4309e6b7c7c6c1e6ea7a78
Jon Eisen <jon@metricfeat.com>
2013-09-24 10:29:35 -0400

    Use a faster lua script for zrangepush

--- scripts/zrangepush.lua
+++ scripts/zrangepush.lua
@@ -1,8 +1,6 @@
 -- KEYS:[from, to], ARGV:[min, max, remove]
 local refs = redis.call("zrangebyscore", KEYS[1], ARGV[1], ARGV[2])
-for i,ref in pairs(refs) do
-  redis.call("lpush", KEYS[2], ref)
-end
+redis.call("lpush", KEYS[2], unpack(refs))
 if ARGV[3] then
   redis.call("zremrangebyscore", KEYS[1], ARGV[1], ARGV[2])
 end
